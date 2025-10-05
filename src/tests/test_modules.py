b155789ff82e37e005bed005178742c0e69bcfa3
Grant Lemons <grantlemons@aol.com>
2025-10-04 23:26:01 -0600

    replace head call in autograder

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -23,8 +23,10 @@ class TestBase(unittest.TestCase):
 
         subprocess.run(['iverilog', '-o', f'/tmp/{name}_test.vvp', f'/autograder/grader/tests/{name}_test.v', f'/autograder/source/{name}.v'])
 
+        ps = subprocess.Popen(['vvp', f'/tmp/{name}_test.vvp'], stdout=subprocess.PIPE)
         out = open(f'/tmp/{name}.out', 'w')
-        subprocess.call(['vvp', f'/tmp/{name}_test.vvp'], stdout=out)
+        subprocess.run(['head', '-n', '-1'], stdin=ps.stdout, stdout=out)
+        ps.wait()
 
         res = subprocess.call(['diff', f'/tmp/{name}.out', f'/autograder/grader/tests/expected-outputs/{name}.cmp', '-qsw', '--strip-trailing-cr'])
         if res != 0:
